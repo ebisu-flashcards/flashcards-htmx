5c0bcb563eb3cf7c653990b978dfa728b1798f0c
ZanSara <sarazanzo94@gmail.com>
2022-07-20 14:07:22 +0200

    Reboot the auth pages, only frontend

--- flashcards_htmx/router.py
+++ flashcards_htmx/router.py
@@ -6,10 +6,24 @@ from fastapi.templating import Jinja2Templates
 
 
 templates = Jinja2Templates(directory=Path(__file__).parent / "templates")
-router = APIRouter(prefix="/home")
+router = APIRouter()
 
 
 @router.get("/")
 async def home(request: Request):
 	return templates.TemplateResponse("public/landing-page.html", {"request":request})
 	
+
+@router.get("/login")
+async def login(request: Request):
+	return templates.TemplateResponse("public/login.html", {"request":request})
+	
+
+@router.get("/signup")
+async def signup(request: Request):
+	return templates.TemplateResponse("public/signup.html", {"request":request})
+
+
+@router.get("/reset-password")
+async def reset_password(request: Request):
+	return templates.TemplateResponse("public/reset-password.html", {"request":request})
