287fd5fc2422fa0a2e5b3b54296d2691e0b099f1
ZanSara <sarazanzo94@gmail.com>
2022-08-02 17:36:51 +0200

    Refine styles and pass mock decks and cards

--- flashcards_htmx/private_components.py
+++ flashcards_htmx/private_components.py
@@ -12,15 +12,29 @@ templates = Jinja2Templates(directory=Path(__file__).parent / "templates")
 router = APIRouter(prefix='/_components')
 
 
+class MockDeck:
+	def __init__(self, index):
+		self.id = str(index)
+		self.name = f"Deck N. {index}"
+		self.desc = f"Description for deck n. {index}"
+		self.algorithm = "Random"
+
+
+class MockCard:
+	def __init__(self, index):
+		self.id = str(index)
+		self.question = f"Question N. {index}"
+		self.answer = f"Answer n. {index}"
+
 
 @router.get("/decks", response_class=HTMLResponse)
 async def decks_component(render = Depends(template('responses/decks.html'))):
-	return render(decks=range(4))
+	return render(decks=[MockDeck(i) for i in range(4)])
 	
 
 @router.get("/decks/{deck_id}/cards", response_class=HTMLResponse)
 async def cards_component(deck_id: str, render = Depends(template('responses/cards.html'))):
-	return render(cards=range(20))
+	return render(deck=MockDeck(deck_id), cards=[MockCard(i) for i in range(20)])
 
 
 
@@ -40,7 +54,7 @@ async def deck_confirm_delete_component(deck_id: str, render = Depends(template(
 
 
 @router.get("/decks/{deck_id}/cards/{card_id}/confirm-delete", response_class=HTMLResponse)
-async def deck_confirm_delete_component(deck_id: str, card_id: str, render = Depends(template('components/confirm_modal.html'))):
+async def card_confirm_delete_component(deck_id: str, card_id: str, render = Depends(template('components/confirm_modal.html'))):
 	return render(
 		title=f"Deleting {card_id}", 
 		content=f"Are you really sure you wanna delete this card?"
--- flashcards_htmx/private_pages.py
+++ flashcards_htmx/private_pages.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pathlib import Path
 
 import starlette.status as status
@@ -7,9 +8,27 @@ from fastapi.templating import Jinja2Templates
 
 from flashcards_htmx.app import template
 
+from flashcards_htmx.private_components import MockDeck, MockCard
+
 
 templates = Jinja2Templates(directory=Path(__file__).parent / "templates")
 router = APIRouter()
+
+
+
+class EmptyDeck:
+	def __init__(self):
+		self.id = None
+		self.name = ""
+		self.desc = ""
+		self.algorithm = None
+
+
+class EmptyCard:
+	def __init__(self):
+		self.id = None
+		self.question = ""
+		self.answer = ""
 
 
 @router.get("/home", response_class=HTMLResponse)
@@ -27,31 +46,57 @@ async def study_page(deck_id: str, render = Depends(template('private/study.html
 	return render(navbar_title=f"{deck_id}")
 		
 
-@router.get("/decks/{deck_id}", response_class=HTMLResponse)
-async def edit_deck_page(deck_id: str, render = Depends(template('private/deck.html'))):
-	return render(navbar_title=f"{deck_id}")
-
-
 @router.get("/decks/new", response_class=HTMLResponse)
 async def create_deck_page(render = Depends(template('private/deck.html'))):
-	return render(navbar_title="New Deck", deck_id="new")
+	return render(navbar_title="New Deck", deck=EmptyDeck())
 		
 
+@router.get("/decks/{deck_id}", response_class=HTMLResponse)
+async def edit_deck_page(deck_id: str, render = Depends(template('private/deck.html'))):
+	deck = MockDeck(deck_id)
+	return render(navbar_title=f"Edit '{deck.name}'", deck=deck)
+
+
 @router.post("/decks/{deck_id}", response_class=RedirectResponse)
-async def save_deck_endpoint(deck_id: str, request: Request):
-	# FIXME save the deck data!!
+async def save_deck_endpoint(deck_id: Optional[str], request: Request):
+	if not deck_id:
+		# FIXME CREATE NEW DECK
+		pass
+	else:
+		# FIXME SAVE CHANGES
+		pass
 	return RedirectResponse(request.url_for('home_page'), status_code=status.HTTP_302_FOUND)
 		
 
 @router.get("/decks/{deck_id}/cards", response_class=HTMLResponse)
 async def cards_page(deck_id: str, render = Depends(template('private/cards.html'))):
-	return render(navbar_title=f"{deck_id}")
+	deck = MockDeck(deck_id)
+	return render(navbar_title=f"{deck.name}", deck=deck)
+
+
+@router.get("/decks/{deck_id}/cards/new", response_class=HTMLResponse)
+async def create_card_page(deck_id: str, render = Depends(template('private/card.html'))):
+	deck = MockDeck(deck_id)
+	return render(navbar_title=f"{deck.name}", deck=deck, card=EmptyCard())
 
 
 @router.get("/decks/{deck_id}/cards/{card_id}", response_class=HTMLResponse)
-async def edit_card_page(deck_id: str, render = Depends(template('private/card.html'))):
-	return render(navbar_title=f"{deck_id}")
+async def edit_card_page(deck_id: str, card_id: str, render = Depends(template('private/card.html'))):
+	deck = MockDeck(deck_id)
+	card = MockCard(card_id)
+	return render(navbar_title=f"{deck.name}", deck=deck, card=card)
 
+
+@router.post("/decks/{deck_id}/cards/{card_id}", response_class=RedirectResponse)
+async def save_card_endpoint(deck_id: str, card_id: Optional[str], request: Request):
+	if not card_id:
+		# FIXME CREATE NEW DECK
+		pass
+	else:
+		# FIXME SAVE CHANGES
+		pass
+	return RedirectResponse(request.url_for('home_page'), status_code=status.HTTP_302_FOUND)
+		
 
 @router.post("/logout", response_class=RedirectResponse)
 async def logout_page(request: Request):
